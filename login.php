e8a5727335d9df51bf6990ad4d61512d88fc8b07
Arihant Golchha <71267935+arujain-789@users.noreply.github.com>
2025-03-29 11:37:27 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Database connection
-$conn = new mysqli("localhost", "root", "", "teacherdetail");
+$conn = new mysqli("34.47.230.235", "EduAI", "Arujain@789", "teacherdetails");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
