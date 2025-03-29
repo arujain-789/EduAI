3e3865691bb8dacfac7d07939cad0a423ff2ffb4
Arihant Golchha <71267935+arujain-789@users.noreply.github.com>
2025-03-29 11:17:09 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = $_POST['subject'];
 
     // Database connection
-    $conn = new mysqli("localhost", "root", "", "teacherdetail");
+    $conn = new mysqli("34.47.230.235", "root", "Arujain@789", "teacherdetails");
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
